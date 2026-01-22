2c0391b6e6db176af49227f78387835ad322679b
Shamshod Abduqayumov <shamshodabduqayumov@gmail.com>
2026-01-22 11:08:55 +0500

    app.jsx o'zgardi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia deserunt
+        tempore ad in dolore iusto tenetur voluptatem aspernatur. Animi fugit
+        voluptatibus temporibus fugiat aperiam natus maiores. Voluptate minima a
+        ut expedita blanditiis dolor cum! Odit vel dicta quasi. Repellat rerum
+        accusantium ipsam sit, expedita iste sint omnis ut esse fugit harum
+        laboriosam, est quo non, officiis maxime molestiae nam recusandae
+        eveniet accusamus autem! Beatae labore iusto eos quia error earum,
+        aliquam totam libero molestiae distinctio quo ut alias maiores animi
+        quod soluta culpa eum voluptatem dolores corrupti ad, magnam vitae!
+        Autem iure excepturi exercitationem quos iusto quidem dicta amet
+        accusamus!
+      </h1>
+    </>
+  );
 }
 
 export default App;
